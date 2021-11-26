11d30e181564051f70d7967d526d79bf1513eca2
João Vitor Fonseca da Silva <joaovfonseca@outlook.com>
2021-11-26 10:31:49 -0300

    Automation

---
+++ README.md
@@ -0,0 +1,8 @@
+#### Useful pytest switches
+| switch | description |
+|-|-|
+| `-s` | dump output to terminal instead of capturing |
+| `-q` | perform the tests quietly |
+| `-x` | stop running tests on first failure |
+| `-m <str>` | run tests marked matching *str* |
+| `-k <str>` | run tests with functions having *str* in the name |  
---
+++ test_via_cep.py
@@ -0,0 +1,26 @@
+import requests
+
+url = 'https://viacep.com.br/ws/'
+
+
+def test_should_call_api_with_cep():
+    cep = '08257060'
+    url_with_cep = url + cep + '/json/'
+    response = requests.get(url_with_cep)
+
+    if response.status_code == 200:
+        print(response.text)
+        assert response.json()['logradouro'] == 'Rua Coração Brasileiro' and \
+               response.json()['cep'].replace('-', '') == cep
+    else:
+        assert False
+
+
+def test_should_call_api_with_invalid_cep():
+    cep = '000000'
+    url_with_cep = url + cep + '/json/'
+    response = requests.get(url_with_cep)
+
+    if response.status_code == 400:
+        print(response.text)
+        assert True
